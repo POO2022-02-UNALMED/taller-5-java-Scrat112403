9a4a1e2eea799ce053402462559f94f00a2613ac
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-09-25 02:47:44 -0500

    A

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -1,88 +1,39 @@
-package zooAnimales;
+public static void setIguanas(int iguanas) {
+    Reptil.iguanas = iguanas;
+}
 
-import java.util.ArrayList;
+public static int getIguanas() {
+    return iguanas;
+}
 
-class Reptil extends Animal {
-    private static ArrayList<Reptil> listado = new ArrayList<Reptil>();
-    public int iguanas;
-    public int serpientes;
-    private String colorEscamas;
-    private int largoCola;
+public static void setSerpientes(int serpientes) {
+    Reptil.serpientes = serpientes;
+}
 
-    public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola) {
-        super(nombre, edad, habitat, genero);
-        this.colorEscamas=colorEscamas;
-        this.largoCola=largoCola;
-        listado.add(this);
-    }
+public static int getSerpientes() {
+    return serpientes;
+}
 
-    public Reptil () {
-        listado.add(this);
-    }
+public void setListado(ArrayList<Reptil> listado) {
+    Reptil.listado = listado;
+}
 
-    @Override
-    public String movimiento() {
-        return "reptar";
-    }
+public ArrayList<Reptil> getListado() {
+    return Reptil.listado;
+}
 
-    public static int cantidadReptiles () {
-        return Ave.listado.size();
-    }
+public void setColorEscamas(String colorEscamas) {
+    this.colorEscamas = colorEscamas;
+}
 
-    public static crearIguana(String nombre, int edad, String genero) {
-        Reptil iguana = new Reptil(nombre, edad, "humedal", genero, "verde", 3);
-        iguanas++;
-        Animal.setTotalAnimales();
-		listado.add(iguana);
-		return iguana;
-    }
+public String getColorEscamas() {
+    return colorEscamas;
+}
 
-    public static crearSerpiente(String nombre, int edad, String genero) {
-        Reptil serpiente = new Reptil(nombre, edad, "jungla", genero, "blanco", 1);
-        serpientes++;
-        Animal.setTotalAnimales();
-		listado.add(serpiente);
-		return serpiente;
-    }
+public void setLargoCola(int largoCola) {
+    this.largoCola = largoCola;
+}
 
-    public static void setIguanas(int iguanas) {
-		Reptil.iguanas = iguanas;
-	}
-
-    public static int getIguanas() {
-		return iguanas;
-	}
-
-    public static void setSerpientes(int serpientes) {
-		Reptil.serpientes = serpientes;
-	}
-
-	public static int getSerpientes() {
-		return serpientes;
-	}
-
-    public void setListado(ArrayList<Reptil> listado) {
-		Reptil.listado = listado;
-	}
-
-	public ArrayList<Reptil> getListado() {
-		return Reptil.listado;
-	}
-    
-	public void setColorEscamas(String colorEscamas) {
-		this.colorEscamas = colorEscamas;
-	}
-
-	public String getColorEscamas() {
-		return colorEscamas;
-	}
-
-    public void setLargoCola(int largoCola) {
-		this.largoCola = largoCola;
-	}
-
-	public int getLargoCola() {
-		return largoCola;
-	}
-
+public int getLargoCola() {
+    return largoCola;
 }
