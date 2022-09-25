fa9136817fa3800c99698654ccbc7b91fc3ad21f
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-09-25 02:51:33 -0500

    a

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -4,53 +4,45 @@ import java.util.ArrayList;
 import zooAnimales.Animal;
 
 public class Zona {
-    private String nombre;
-    private Zoologico zoo;
-    private ArrayList<Animal> animales = new ArrayList<Animal>();
+	private String nombre;
+	private Zoologico zoo;
+	private ArrayList<Animal> animales = new ArrayList<Animal>();
+	
+	public Zona(String nombre, Zoologico zoo) {
+		this.nombre = nombre;
+		this.zoo = zoo;
+	}
+	public Zona() {
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Zoologico getZoo() {
+		return zoo;
+	}
+	public void setZoo(Zoologico zoo) {
+		this.zoo = zoo;
+	}
 
-    public Zona(String nombre, Zoologico zoo) {
-        this.nombre = nombre;
-        this.zoo = zoo;
-    }
-
-    public Zona() {
-
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setZoo(Zoologico zoo) {
-        this.zoo= zoo;
-    }
-
-    public getZoo() {
-        return zoo;
-    }
-
-    public void agregarAnimales(Animal animal) {
-        animales.add(animal):
-        animal.setZona(this);
-    }
-
-    public ArrayList<Animal> getAnimales() {
-        return animales;
-    }
-
-    public void setAnimales(ArrayList<Animal> animales) {
-        this.animales=animales;
-    }
-
-    public int cantidadAnimales() {
-        return animales.size();
-    }
-
-    public String toString() {
-        return nombre;
-    }
+	public void agregarAnimales(Animal animal) {
+		animales.add(animal);
+		animal.setZona(this);
+	}
+	
+	public ArrayList<Animal> getAnimales() {
+		return animales;
+	}
+	public void setAnimales(ArrayList<Animal> animales) {
+		this.animales = animales;
+	}
+	public int cantidadAnimales() {
+		return animales.size();
+	}
+	
+	public String toString() {
+		return nombre;
+	}
 }
